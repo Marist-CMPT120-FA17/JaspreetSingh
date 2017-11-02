eb5875c8bca233d3321b7bc21efebd0fe84efb7b
Jaspreetsingh15 <31972855+Jaspreetsingh15@users.noreply.github.com>
2017-11-01 20:30:44 -0400

    Add files via upload

---
+++ location.js
@@ -0,0 +1,75 @@
+function livingroom() {
+   var desc = "The player heads to the living room and ends up seeing an axe on the bookcase, which he picks up.";
+   checkScore();
+   desc = desc + "\n" + "Score: " + score ;
+   displayMessage(desc);
+}
+
+function entrance() {
+   var desc = "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe.";
+   checkScore();
+   desc = desc + "\n" + "Score: " + score ; 
+   displayMessage(desc);
+}
+
+function steps() {
+   var desc = "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps.";
+   checkScore();
+   desc = desc + "\n" + "Score: " + score ;
+   displayMessage(desc);
+}
+
+function hallway() {
+   var desc = "When the player reaches the top of the steps, he kicks down the door and enters the bedroom.";
+   checkScore();
+   desc = desc + "\n" + "Score: " + score ;
+   displayMessage(desc);
+}
+function room1() {
+   var desc = "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all.";
+   checkScore();
+   desc = desc + "\n" + "Score: " + score ;
+   displayMessage(desc);
+}
+function bathroom() {
+    var desc = "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat."; 
+    checkScore();
+    desc = desc + "\n" + "Score: " + score ;
+    displayMessage(desc);
+}
+function room2() {
+    var desc = "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat."; 
+    checkScore();
+    desc = desc + "\n" + "Score: " + score ;
+    displayMessage(desc);
+}    
+
+function attic() {
+    var desc =  "Then the player spots something shining in the attic which the player grabs which is a machete.;"
+    checkScore();
+    desc = desc + "\n" + "Score: " + score ;
+    displayMessage(desc);
+}
+function diningroom() {
+    var desc = "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete.";
+    checkscore();
+    desc = desc + "\n" + "Score: " + score ;
+    displayMessage(desc);
+
+}
+function outside() {
+   var des = "Finally, the player walks out the front door of the house with excitement because he killed all the zombies.";
+   checkscore(); 
+   desc = desc + "\n" + "Score: " + score ;
+   displayMessage(desc);
+
+}  
+
+
+
+
+
+
+
+
+
