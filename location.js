da95f9aab32f3f76066df3cf1c80dbe001bf9f02
Jaspreetsingh15 <31972855+Jaspreetsingh15@users.noreply.github.com>
2017-11-09 19:29:14 -0500

    Add files via upload

--- location.js
+++ location.js
@@ -1,75 +1,181 @@
 function livingroom() {
-   var desc = "The player heads to the living room and ends up seeing an axe on the bookcase, which he picks up.";
-   checkScore();
-   desc = desc + "\n" + "Score: " + score ;
-   updatedisplay(message);
-}
+  if (Loc1 == false) {
+    Loc1 = true;
+    uscore += 5;
+    updateScore();
+    var message = "The player heads to the living room and ends up seeing an axe on the bookcase, which he picks up.";
+    updateDisplay(message);
+  } else {
+    var message = "The player heads to the living room and ends up seeing an axe on the bookcase, which he picks up.";
+    updateDisplay(message);
+  }
+  document.getElementById("North").disabled = true;
+  document.getElementById("South").disabled = false;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = true;
+}  
 
 function entrance() {
-   var desc = "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe.";
-   checkScore();
-   desc = desc + "\n" + "Score: " + score ; 
-   updatedisplay(message);
-}
+    if (Loc2 == false) {
+    Loc2 = true;
+    uscore += 5;
+    updateScore();
+   var message = "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe.";
+   updateDisplay(message);
+ } else {
+     var message = "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe.";
+     updateDisplay(message);
+ }
+ 
+   updateDisplay(message);
 
+  document.getElementById("North").disabled = true;
+  document.getElementById("South").disabled = false;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = false;
+}
 function steps() {
-   var desc = "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps.";
-   checkScore();
-   desc = desc + "\n" + "Score: " + score ;
-   updatedisplay(message);
+  if (Loc3 == false) {
+    Loc3 = true;
+    uscore += 5;
+    updateScore();
+   var message = "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps.";
+   updateDisplay(message);
+
+   
+ } else {
+     var message = "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps.";
+     updateDisplay(message);
+ }
+
+  document.getElementById("North").disabled = true;
+  document.getElementById("South").disabled = false;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = false;
 }
 
 function hallway() {
-   var desc = "When the player reaches the top of the steps, he kicks down the door and enters the bedroom.";
-   checkScore();
-   desc = desc + "\n" + "Score: " + score ;
-   updatedisplay(message);
+  if (Loc4 == false) {
+    Loc4 = true;
+    uscore += 5;
+    updateScore();
+   var message = "When the player reaches the top of the steps, he kicks down the door and enters the bedroom.";
+
+   updateDisplay(message);
+} else {
+     var message = "When the player reaches the top of the steps, he kicks down the door and enters the bedroom.";
+     updateDisplay(message);
+
+  document.getElementById("North").disabled = true;
+  document.getElementById("South").disabled = false;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = false;
+}
 }
 function room1() {
-   var desc = "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all.";
-   checkScore();
-   desc = desc + "\n" + "Score: " + score ;
-   updatedisplay(message);
+  if (Loc5 == false) {
+    Loc5 = true;
+    uscore += 5;
+    updateScore();
+    var message = "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all.";
+    updateDisplay(message);
+} else {
+    var message = "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all.";
+    updateDisplay(message);
 }
+
+  document.getElementById("North").disabled = true;
+  document.getElementById("South").disabled = false;
+  document.getElementById("East").disabled = true;
+  document.getElementById("West").disabled = false;
+}
+
 function bathroom() {
-    var desc = "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat."; 
-    checkScore();
-    desc = desc + "\n" + "Score: " + score ;
-    updatedisplay(message);
+  if (Loc6 == false) {
+    Loc6 = true;
+    uscore += 5;
+    updateScore();
+    var message = "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat."; 
+    updateDisplay(message);
+} else {
+    var message = "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat."; 
+    updateDisplay(message);
 }
+  document.getElementById("North").disabled = false;
+  document.getElementById("South").disabled = true;
+  document.getElementById("East").disabled = true;
+  document.getElementById("West").disabled = false;
+}
+
 function room2() {
-    var desc = "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat."; 
-    checkScore();
-    desc = desc + "\n" + "Score: " + score ;
-    updatedisplay(message);
-}    
+  if (Loc7 == false) {
+    Loc7 = true;
+    uscore += 5;
+    updateScore();
+    var message = "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat."; 
+
+    updateDisplay(message);
+} else {
+    var message = "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat."; 
+    updateDisplay(message);
+}
+
+  document.getElementById("North").disabled = false;
+  document.getElementById("South").disabled = true;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = false;
+}
 
 function attic() {
-    var desc =  "Then the player spots something shining in the attic which the player grabs which is a machete.;"
-    checkScore();
-    desc = desc + "\n" + "Score: " + score ;
-    updatedisplay(message);
+   if (Loc8 == false) {
+    Loc8 = true;
+    uscore += 5;
+    updateScore();
+    var message =  "Then the player spots something shining in the attic which the player grabs which is a machete.;"
+
+    updateDisplay(message);    
+} else {
+   var message =  "Then the player spots something shining in the attic which the player grabs which is a machete.;"
+   updateDisplay(message);    
+
+}
+  document.getElementById("North").disabled = false;
+  document.getElementById("South").disabled = true;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = false;
 }
 function diningroom() {
-    var desc = "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete.";
-    checkscore();
-    desc = desc + "\n" + "Score: " + score ;
-    updatedisplay(message);
+  if (Loc9 == false) {
+    Loc9 = true;
+    uscore += 5;
+    updateScore();
+    var message = "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete.";
+    updateDisplay(message);
+} else {
+    var message = "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete.";
+    updateDisplay(message);
 
 }
+
+  document.getElementById("North").disabled = false;
+  document.getElementById("South").disabled = true;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = false;
+}
 function outside() {
-   var des = "Finally, the player walks out the front door of the house with excitement because he killed all the zombies.";
-   checkscore(); 
-   desc = desc + "\n" + "Score: " + score ;
-   updatedisplay(message);
+  if (Loc10 == false) {
+    Loc10 = true;
+    uscore += 5;
+    updateScore();
+   var message = "Finally, the player walks out the front door of the house with excitement because he killed all the zombies.";
+   updateDisplay(message);
+}  else {
+   var message = "Finally, the player walks out the front door of the house with excitement because he killed all the zombies.";
+   updateDisplay(message);
 
-}  
-
-
-
-
-
-
-
-
-
+}
+  document.getElementById("North").disabled = false;
+  document.getElementById("South").disabled = true;
+  document.getElementById("East").disabled = false;
+  document.getElementById("West").disabled = true;
+}
