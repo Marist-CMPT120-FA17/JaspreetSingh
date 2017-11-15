5a8e2367632a5530f777efa0ae6e51d8e47ad547
Jaspreetsingh15 <31972855+Jaspreetsingh15@users.noreply.github.com>
2017-11-15 17:41:36 -0500

    Add files via upload

--- location.js
+++ location.js
@@ -1,43 +1,42 @@
-function Location(id,name,description)
+gItemsArray = new Array();
+
+function items(id, name, description)
+   {
+       this.id = id;
+       this.name = name;
+       this.description = description;
+       this.toString = function () {return(this.id + "" + this.name + "" + this.description); };
+   }
+
+function Location(id,name,description, item)
 {
-  this.id = id;
-  this.name = name;
-  this.description = description;
-
-function tostring() 
-{ 
-  return(this.id + "" + this.name + "" + this.description); 
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.item = item;
+    this.toString = function () {return(this.id + "" + this.name + "" + this.description); };
 }
-  locationarray = new Array();
+ 
+//initialize items
+var item1 = new items(0, "Hardware", "You found an Axe");
+var item2 = new items(2, "Gun", "You found an Ak-47");
+var item3 = new items(5, "Bat", "You found a Baseball Bat");
+var item4 = new items(7, "Knife", "You found a Machete");
 
-   var livingroom(new location(1,"livingroom","The player heads to the living room and ends up seeing an Axe on the bookcase, which he picks up."));
-   var entrance(new location(2,"entrance", "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe."));
-   var steps(new location(3, "steps", "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps."));
-   var hallway(new location(4, "hallway", "When the player reaches the top of the steps, he kicks down the door and enters the bedroom."));
-   var room1(new location(5, "room1",  "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all."));
-   var bathroom(new location(6, "bathroom", "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat."));
-   var room2(new location(7, "room2", "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat."));
-   var attic(new location(8, "attic", "Then the player spots something shining in the attic which the player grabs which is a Machete."));
-   var diningroom(new location(9, "diningroom", "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete."));
-   var outside(new location(10, "outside", "Finally, the player walks out the front door of the house with excitement because he killed all the zombies."));5
+//intitialize locations
+var l_livingroom = new Location(0,"livingroom","The player heads to the living room and ends up seeing an Axe on the bookcase, which he picks up.", item1);
+var l_entrance = new Location(1,"entrance", "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe.");
+var l_steps = new Location(2, "steps", "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps.", item2);
+var l_hallway = new Location(3, "hallway", "When the player reaches the top of the steps, he kicks down the door and enters the bedroom.");
+var l_room1 = new Location(4, "room1", "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all.");
+var l_bathroom = new Location(5, "bathroom", "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat.", item3);
+var l_room2 = new Location(6, "room2", "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat.");
+var l_attic = new Location(7, "attic", "Then the player spots something shining in the attic which the player grabs which is a Machete.", item4);
+var l_diningroom = new Location(8, "diningroom", "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete.");
+var l_outside = new Location(9, "outside", "Finally, the player walks out the front door of the house with excitement because he killed all the zombies.");
 
-function items(id,name,description)
-{
-  this.id = id;
-  this.name = name;
-  this.description = description;
-
-function tostring()
-{
-  return(this.id + "" + this.name + "" + this.description);
-}
-  itemsarray = new Array();
-
-  var item1(new items(1, "Hardware", "You found an Axe"));
-  var item2(new items(3, "Gun", "You found an Ak-47"));
-  var item3(new items(6, "Bat", "You found a Baseball Bat"));
-  var item4(new items(8, "Knife", "You found a Machete"));
-}
+//intitialize global location array
+var gLocationArray = [l_livingroom, l_entrance, l_steps, l_hallway, l_room1, l_bathroom, l_room2, l_attic, l_diningroom, l_outside];
 
 
 
