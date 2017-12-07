25b4867ada50bd4c419fd1f09e65c5b05e65297f
Jaspreetsingh15 <31972855+Jaspreetsingh15@users.noreply.github.com>
2017-12-07 16:40:41 -0500

    Update location.js

--- location.js
+++ location.js
@@ -24,16 +24,16 @@ var item3 = new items(5, "Baseball Bat", "You found a Baseball Bat");
 var item4 = new items(7, "Machete", "You found a Machete");
 
 //intitialize locations
-var l_livingroom = new Location(0,"livingroom","The player heads to the living room and ends up seeing an Axe on the bookcase, which he picks up.Take Axe.", item1);
-var l_entrance = new Location(1,"entrance", "While heading for the steps, two zombies approach him making him use the only weapon in hand which is the axe.");
-var l_steps = new Location(2, "steps", "After killing the two zombies, the player stumbles upon a fully loaded AK-47 heading up the steps. Take Ak-47.", item2);
+var l_livingroom = new Location(0,"livingroom","The player heads to the living room. There is an Axe on the bookcase, which the player can pick up.", item1);
+var l_entrance = new Location(1,"entrance", "While heading for the steps, two zombies approach the player. The player may kill the zombies with a random weapon automatically selected from their inventory.");
+var l_steps = new Location(2, "steps", "The player heads to the steps. The player sees a fully loaded AK-47 heading up the steps, which the player can pick up.", item2);
 var l_hallway = new Location(3, "hallway", "When the player reaches the top of the steps, he kicks down the door and enters the bedroom.");
-var l_room1 = new Location(4, "room1", "The player sees the 20 zombies in the bedroom and decides to use the Ak-47 to kill them all.");
-var l_bathroom = new Location(5, "bathroom", "Then the player heads to the bathroom in search of more weapons and stumbles upon a baseball bat. Take Baseball Bat.", item3);
-var l_room2 = new Location(6, "room2", "While entering the second room, the player spots 2 zombies, and knocks the heads of both of them using the bat.");
-var l_attic = new Location(7, "attic", "Then the player spots something shining in the attic which the player grabs which is a Machete. Take Machete.", item4);
-var l_diningroom = new Location(8, "diningroom", "The player spots 4 zombies eating food from the table, when they see the player they try attacking him but the player kills them all with the machete.");
-var l_outside = new Location(9, "outside", "Finally, the player walks out the front door of the house with excitement because he killed all the zombies.");
+var l_room1 = new Location(4, "room1", "The player sees the 20 zombies in the bedroom. The player may kill the zombies with a random weapon automatically selected from their inventory.");
+var l_bathroom = new Location(5, "bathroom", "The player heads to the bathroom in search of more weapons and stumbles upon a baseball bat, which the player may pick up.", item3);
+var l_room2 = new Location(6, "room2", "While entering the second room, the player spots 2 zombies. The player may kill the zombies with a random weapon automatically selected from their inventory.");
+var l_attic = new Location(7, "attic", "Then the player spots something shining in the attic. It is a Machete, which the player can pick up.", item4);
+var l_diningroom = new Location(8, "diningroom", "The player spots 4 zombies eating food from the table, when they see the player they try attacking him. The player may kill the zombies with a random weapon automatically selected from their inventory.");
+var l_outside = new Location(9, "outside", "Finally, the player walks out of the zombie ridden house.");
 
 //intitialize global location array
 var gLocations = [l_livingroom, l_entrance, l_steps, l_hallway, l_room1, l_bathroom, l_room2, l_attic, l_diningroom, l_outside];
